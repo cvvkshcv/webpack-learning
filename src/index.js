198abece406e18e0ff6665cc9be470d00c6b6c71
Vikash UI_GEMS <cvvkshcv@gmail.com>
2023-01-20 19:31:02 +0530

    added css loader and css modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import _ from 'lodash';
+import style from './index.css';
+
+console.log({style});
+
+const add = require('./functions/add');
+const sub = require('./functions/sub');
+
+console.log(add(1, 2));
 
 document.getElementById('btn').addEventListener('click', () => {
-  document.getElementById('h1').innerText = _.random(1000);
+  const h1 = document.getElementById('h1')
+  h1.innerText = _.random(1000);
+  h1.className = style.button;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { modules: true } }
+        ]
+      }
+    ]
   }
 };
